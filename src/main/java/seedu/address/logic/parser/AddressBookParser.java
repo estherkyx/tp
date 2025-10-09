d964df278337611efbc1a6e06119f002a6d7609a
oysterboi <yanshengong@gmail.com>
2025-10-09 23:47:28 +0800

    Fix checkstyle issues again

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -17,8 +17,8 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.GetStudentsCommand;
 import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.LinkTutorCommand;
 import seedu.address.logic.commands.LinkParentCommand;
+import seedu.address.logic.commands.LinkTutorCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
