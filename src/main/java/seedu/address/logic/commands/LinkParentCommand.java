9adbaf2abbcedd26c3db45f1a48a76e04cb0c3a0
reyna <171039150+reynabxr@users.noreply.github.com>
2025-10-09 17:04:47 +0800

    Create LinkParentCommand and implement parse user input

---
+++ src/main/java/seedu/address/logic/commands/LinkParentCommand.java
@@ -0,0 +1,56 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Name;
+
+public class LinkParentCommand extends Command {
+    public static final String COMMAND_WORD = "linkParent";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": links a Student to a Tutor "
+            + "Parameters: "
+            + PREFIX_NAME + "NAME "
+            + PREFIX_NAME + "NAME "
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + "John Doe "
+            + PREFIX_NAME + "Amy Tan";
+
+
+    public static final String MESSAGE_ARGUMENTS = "Student: %1$s, Parent: %2$s";
+
+    private final Name studentName;
+    private final Name parentName;
+
+    /**
+     * @param studentName of the student to be linked
+     * @param parentName of the parent to be linked
+     */
+    public LinkParentCommand(Name studentName, Name parentName) {
+        requireAllNonNull(studentName, parentName);
+        this.studentName = studentName;
+        this.parentName = parentName;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(String.format(MESSAGE_ARGUMENTS, studentName, parentName));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (!(other instanceof LinkParentCommand)) {
+            return false;
+        }
+
+        LinkParentCommand e = (LinkParentCommand) other;
+        return studentName.equals(e.studentName)
+                && parentName.equals(e.parentName);
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/LinkParentCommandParser.java
@@ -0,0 +1,41 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+
+import java.util.List;
+
+import seedu.address.logic.commands.LinkParentCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Name;
+
+public class LinkParentCommandParser implements Parser<LinkParentCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the LinkParentCommand
+     * and returns a LinkParentCommand object for execution.
+     * @throws ParseException if the user input does not conform to the expected format
+     */
+    public LinkParentCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME);
+
+        // Ensure that exactly two names are provided
+        if (argMultimap.getAllValues(PREFIX_NAME).size() != 2) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, LinkParentCommand.MESSAGE_USAGE));
+        }
+
+        // Preamble should be empty
+        if (!argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, LinkParentCommand.MESSAGE_USAGE));
+        }
+
+        List<String> names = argMultimap.getAllValues(PREFIX_NAME);
+        Name studentName = ParserUtil.parseName(names.get(0));
+        Name parentName = ParserUtil.parseName(names.get(1));
+
+        return new LinkParentCommand(studentName, parentName);
+    }
+}
+
+
