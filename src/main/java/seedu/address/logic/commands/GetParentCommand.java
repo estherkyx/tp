74fec3039d448039ca9722b270dcb3679f04961c
zzzyans <limzhiyan05@gmail.com>
2025-10-15 22:24:26 +0800

    Implement full getParent command and corresponding tests

--- src/main/java/seedu/address/logic/commands/GetParentCommand.java
+++ src/main/java/seedu/address/logic/commands/GetParentCommand.java
@@ -2,9 +2,13 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.List;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.PersonId;
 
 /**
  * Retrieves the parent of a specified student in the address book.
@@ -16,7 +20,9 @@ public class GetParentCommand extends Command {
             + "Parameters: n/STUDENT_NAME\n"
             + "Example: " + COMMAND_WORD + " " + " n/John Doe";
 
-    public static final String MESSAGE_ARGUMENTS = "Student: %1$s";
+    public static final String MESSAGE_SUCCESS = "Parent of %s:\n%s";
+    public static final String MESSAGE_STUDENT_NOT_FOUND = "Student with name '%s' not found.";
+    public static final String MESSAGE_NO_PARENT_LINKED = "Student '%s' has no linked parent.";
 
     private final Name studentName;
 
@@ -31,7 +37,41 @@ public class GetParentCommand extends Command {
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        throw new CommandException(String.format(MESSAGE_ARGUMENTS, studentName));
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+        // Find the student with the matching name
+        Person targetStudent = null;
+        for (Person person : lastShownList) {
+            if (person.getCategory().equals("student") && person.getName().equals(studentName)) {
+                targetStudent = person;
+                break;
+            }
+        }
+
+        // If student is not found, throw an exception
+        if (targetStudent == null) {
+            throw new CommandException(String.format(MESSAGE_STUDENT_NOT_FOUND, studentName));
+        }
+
+        // Get the parent ID from the student
+        PersonId parentId = targetStudent.getParentId();
+
+        // If student has no linked parent, throw exception
+        if (parentId == null) {
+            throw new CommandException(String.format(MESSAGE_NO_PARENT_LINKED, studentName));
+        }
+
+        // Find parent with the matching ID
+        Person targetParent = null;
+        for (Person person : lastShownList) {
+            if (person.getCategory().equals("parent") && person.getId().equals(parentId)) {
+                targetParent = person;
+                break;
+            }
+        }
+
+        // Return parent details
+        return new CommandResult(String.format(MESSAGE_SUCCESS, studentName, targetParent));
     }
 
     @Override
--- src/main/java/seedu/address/model/person/Student.java
+++ src/main/java/seedu/address/model/person/Student.java
@@ -33,6 +33,7 @@ public class Student extends Person {
         this.linkedTutorId = null;
     }
 
+    @Override
     public PersonId getParentId() {
         return linkedParentId;
     }
