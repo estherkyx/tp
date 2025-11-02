670596fa5a22e83d05777e5a18c795eb2be0fda2
reyna <171039150+reynabxr@users.noreply.github.com>
2025-11-03 02:04:32 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/UnlinkClassCommand.java
+++ src/main/java/seedu/address/logic/commands/UnlinkClassCommand.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.isNull;
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_CATEGORY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DAY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME;
