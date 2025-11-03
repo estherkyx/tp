3ac990b85f72c04460f163ae37417efcdadb8b89
oysterboi <yanshengong@gmail.com>
2025-11-03 18:48:58 +0800

    Remove category parameter from linkClass

--- src/main/java/seedu/address/logic/parser/LinkClassCommandParser.java
+++ src/main/java/seedu/address/logic/parser/LinkClassCommandParser.java
@@ -2,7 +2,6 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_CATEGORY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DAY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME;
@@ -11,7 +10,6 @@ import java.util.stream.Stream;
 
 import seedu.address.logic.commands.LinkClassCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.Category;
 import seedu.address.model.person.Name;
 import seedu.address.model.tuitionclass.Day;
 import seedu.address.model.tuitionclass.Time;
@@ -29,26 +27,21 @@ public class LinkClassCommandParser implements Parser<LinkClassCommand> {
     public LinkClassCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_DAY, PREFIX_TIME, PREFIX_NAME, PREFIX_CATEGORY);
+                ArgumentTokenizer.tokenize(args, PREFIX_DAY, PREFIX_TIME, PREFIX_NAME);
 
         if (!arePrefixesPresent(argMultimap, PREFIX_DAY, PREFIX_TIME, PREFIX_NAME)
                 || !argMultimap.getPreamble().trim().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, LinkClassCommand.MESSAGE_USAGE));
         }
 
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_DAY, PREFIX_TIME, PREFIX_NAME, PREFIX_CATEGORY);
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_DAY, PREFIX_TIME, PREFIX_NAME);
 
         try {
             Day day = ParserUtil.parseDay(argMultimap.getValue(PREFIX_DAY).get());
             Time time = ParserUtil.parseTime(argMultimap.getValue(PREFIX_TIME).get());
             Name studentName = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
 
-            var opt = argMultimap.getValue(PREFIX_CATEGORY);
-            if (opt.isEmpty()) {
-                return new LinkClassCommand(day, time, studentName);
-            }
-            Category category = ParserUtil.parseCategory(opt.get());
-            return new LinkClassCommand(day, time, studentName, category);
+            return new LinkClassCommand(day, time, studentName);
         } catch (IllegalArgumentException e) {
             throw new ParseException(e.getMessage());
         }
--- src/main/java/seedu/address/logic/parser/UnlinkClassCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UnlinkClassCommandParser.java
@@ -2,17 +2,14 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_CATEGORY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DAY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME;
 
-import java.util.Optional;
 import java.util.stream.Stream;
 
 import seedu.address.logic.commands.UnlinkClassCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.Category;
 import seedu.address.model.person.Name;
 import seedu.address.model.tuitionclass.Day;
 import seedu.address.model.tuitionclass.Time;
@@ -25,25 +22,21 @@ public class UnlinkClassCommandParser implements Parser<UnlinkClassCommand> {
     public UnlinkClassCommand parse(String userInput) throws ParseException {
         requireNonNull(userInput);
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(userInput, PREFIX_DAY, PREFIX_TIME, PREFIX_NAME, PREFIX_CATEGORY);
+                ArgumentTokenizer.tokenize(userInput, PREFIX_DAY, PREFIX_TIME, PREFIX_NAME);
 
         if (!arePrefixesPresent(argMultimap, PREFIX_DAY, PREFIX_TIME, PREFIX_NAME)
                 || !argMultimap.getPreamble().trim().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnlinkClassCommand.MESSAGE_USAGE));
         }
 
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_DAY, PREFIX_TIME, PREFIX_NAME, PREFIX_CATEGORY);
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_DAY, PREFIX_TIME, PREFIX_NAME);
 
         try {
             Day day = ParserUtil.parseDay(argMultimap.getValue(PREFIX_DAY).get());
             Time time = ParserUtil.parseTime(argMultimap.getValue(PREFIX_TIME).get());
             Name studentName = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
-            Optional<String> opt = argMultimap.getValue(PREFIX_CATEGORY);
-            if (opt.isEmpty()) {
-                return new UnlinkClassCommand(day, time, studentName);
-            }
-            Category category = ParserUtil.parseCategory(opt.get());
-            return new UnlinkClassCommand(day, time, studentName, category);
+
+            return new UnlinkClassCommand(day, time, studentName);
         } catch (IllegalArgumentException e) {
             throw new ParseException(e.getMessage());
         }
